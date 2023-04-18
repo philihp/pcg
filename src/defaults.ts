d5022df4f581cc862de5909e72d1a117d835069d
Philihp Busby <philihp@gmail.com>
2023-04-18 23:25:05 +0000

    swap js for ts

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,6 +1,5 @@
 import Long from 'long'
-import { XSH_RR } from './enums/OutputFnType'
-import { SETSEQ } from './enums/StreamScheme'
+import { StreamScheme, OutputFnType } from './types'
 
 // export const pcgDefaultIncrement8 = 77;
 // export const pcgDefaultIncrement16 = 47989;
@@ -12,5 +11,5 @@ export const pcgDefaultIncrement64 = Long.fromString('1442695040888963407', true
 // export const pcgDefaultMultiplier32 = 747796405;
 export const pcgDefaultMultiplier64 = Long.fromString('6364136223846793005', true)
 
-export const pcgDefaultOutputFnType = XSH_RR
-export const pcgDefaultStreamScheme = SETSEQ
+export const pcgDefaultOutputFnType: OutputFnType = OutputFnType.XSH_RR
+export const pcgDefaultStreamScheme: StreamScheme = StreamScheme.SETSEQ
