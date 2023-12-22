90f8ccf661c20b69a34c3d7c016c06b8b57a5718
‮Philihp Busby <philihp@gmail.com>
2023-12-22 20:41:51 +0000

    feature: add XSH_RS, XSH_RR, and RXS_M_XS output functions (#116)

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,15 +1,15 @@
 import Long from 'long'
 import { StreamScheme, OutputFnType } from './types'
 
-// export const pcgDefaultIncrement8 = 77;
-// export const pcgDefaultIncrement16 = 47989;
-// export const pcgDefaultIncrement32 = 2891336453;
-export const pcgDefaultIncrement64 = Long.fromString('1442695040888963407', true)
+// export const pcgDefaultIncrement8 = 77
+// export const pcgDefaultIncrement16 = 47989
+// export const pcgDefaultIncrement32 = Long.fromString('2891336453', 10)
+export const pcgDefaultIncrement64 = Long.fromString('1442695040888963407', 10)
 
-// export const pcgDefaultMultiplier8 = 141;
-// export const pcgDefaultMultiplier16 = 12829;
-// export const pcgDefaultMultiplier32 = 747796405;
-export const pcgDefaultMultiplier64 = Long.fromString('6364136223846793005', true)
+// export const pcgDefaultMultiplier8 = 141
+// export const pcgDefaultMultiplier16 = 12829
+// export const pcgDefaultMultiplier32 = Long.fromString('747796405')
+export const pcgDefaultMultiplier64 = Long.fromString('6364136223846793005', 10)
 
 export const pcgDefaultOutputFnType: OutputFnType = OutputFnType.XSH_RR
 export const pcgDefaultStreamScheme: StreamScheme = StreamScheme.SETSEQ
--- src/index.ts
+++ src/index.ts
@@ -13,5 +13,16 @@ export const createPcg32 = createPcg({
   outputFns: {
     [OutputFnType.XSH_RR]: (state: Long): number =>
       ror32(state.shru(59).toInt(), state.shru(18).xor(state).shru(27).toInt()),
+    [OutputFnType.XSH_RS]: (state: Long): number => state.shru(22).xor(state).shru(state.shru(61).add(22)).toInt(),
+    [OutputFnType.XSL_RR]: (state: Long): number => ror32(state.shru(59).toInt(), state.shru(32).xor(state).toInt()),
+    // [OutputFnType.XSL_RR_RR]: (state: Long): number => {
+    //   const high = state.shru(32)
+    //   const newlow = ror32(state.shru(59).toInt(), high.xor(state).toInt())
+    //   return new Long(ror32(new Long(newlow).and(32).toInt(), high.toInt())).shl(32).or(newlow).toInt()
+    // },
+    [OutputFnType.RXS_M_XS]: (state: Long): number => {
+      const word = state.shru(13).add(3).xor(state).mul(62169)
+      return word.shru(11).xor(word).toInt()
+    },
   },
 })
